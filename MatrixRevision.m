58d1cbf32089451d522e1e945fdb28f0fb14f0ac
root <abelardccwang@gmail.com>
2019-03-07 15:47:07 +0900

    course works from Coursera

--- MatrixRevision.m
+++ MatrixRevision.m
@@ -42,3 +42,26 @@ AB = A*B
 BA = B*A 
 
 % Note that IA = AI but AB != BA
+
+% matrix and inverse of matrix 
+
+A = [3, 4 ; 2, 16]
+
+inverseofA = pinv(A)
+
+A * inverseofA
+
+
+% Initialize matrix A 
+A = [1,2,0;0,5,6;7,0,9]
+
+% Transpose A 
+A_trans = A' 
+
+% Take the inverse of A 
+A_inv = inv(A)
+
+% What is A^(-1)*A? 
+A_invA = inv(A)*A
+
+
